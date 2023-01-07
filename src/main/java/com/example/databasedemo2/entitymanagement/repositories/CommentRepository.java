b9539bca087dd55bac40741f41aa53a6bf7ee83b
Marcin Lenkiewicz <marcinlenki@wp.pl>
2023-01-07 23:31:34 +0100

    * dodanie dodatkowych parametrów zapytań

--- src/main/java/com/example/databasedemo2/entitymanagement/repositories/CommentRepository.java
+++ src/main/java/com/example/databasedemo2/entitymanagement/repositories/CommentRepository.java
@@ -1,6 +1,16 @@
 package com.example.databasedemo2.entitymanagement.repositories;
 
 import com.example.databasedemo2.entitymanagement.entities.Comment;
+import jakarta.validation.constraints.Email;
+
+import java.util.Collections;
+import java.util.Set;
 
 public interface CommentRepository extends BaseRepository<Comment, Integer> {
+    @Override
+    default Set<Comment> findAllWithParam(String key, String val) {
+        return key.equals("user") ?  findAllByUser_Email(val) : Collections.emptySet();
+    }
+
+    Set<Comment> findAllByUser_Email(@Email(message = "Not valid email address!") String user_email);
 }
