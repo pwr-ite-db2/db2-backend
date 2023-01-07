b94f3350da71da5f749f7542e78f0ed3787975b8
Marcin Lenkiewicz <marcinlenki@wp.pl>
2023-01-07 23:25:10 +0100

    * naprawione wyszukiwanie artykułów po tagach

--- src/main/java/com/example/databasedemo2/entitymanagement/repositories/ArticleRepository.java
+++ src/main/java/com/example/databasedemo2/entitymanagement/repositories/ArticleRepository.java
@@ -1,9 +1,11 @@
 package com.example.databasedemo2.entitymanagement.repositories;
 
 import com.example.databasedemo2.entitymanagement.entities.Article;
-import com.example.databasedemo2.entitymanagement.entities.Tag;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public interface ArticleRepository extends BaseRepository<Article, Integer> {
 
@@ -12,24 +14,22 @@ public interface ArticleRepository extends BaseRepository<Article, Integer> {
         return switch (key) {
             case "category" ->  findAllByCategory_Id(Integer.parseInt(val));
             case "author" -> findAllByAuthor_Name(val);
-            case "tags" -> findAllByTagsIn(createTagsFromString(val));
+            case "tags" -> findByTags_IdIn(createTagsFromString(val));
             default -> Collections.emptySet();
         };
     }
 
     Set<Article> findAllByCategory_Id(int categoryId);
     Set<Article> findAllByAuthor_Name(String name);
-    Set<Article> findAllByTagsIn(Collection<Set<Tag>> tags);
+    Set<Article> findByTags_IdIn(Collection<Integer> tags_id);
 
-    private Collection<Set<Tag>> createTagsFromString(String tags) {
-        Set<Tag> returnSet = new HashSet<>();
-        String [] tagsArr = tags.substring(tags.indexOf('{'), tags.indexOf('}')).split(",");
-
-        System.out.println(Arrays.toString(tagsArr));
+    private Collection<Integer> createTagsFromString(String tags) {
+        Set<Integer> returnSet = new HashSet<>();
+        String [] tagsArr = tags.substring(tags.indexOf('=') + 1).split(",");
 
         for (String tagId : tagsArr)
-            returnSet.add(Tag.builder().id(Integer.parseInt(tagId)).build());
+            returnSet.add(Integer.parseInt(tagId));
 
-        return List.of(returnSet);
+        return returnSet;
     }
 }
